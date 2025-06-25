5a136cf61d255313d8e1e52a3cff38d058e0474a
farisZidan <fariszidan30@gmail.com>
2025-06-25 23:26:11 +0700

    2

--- 1.c
+++ 1.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
-void main() {
-    printf("Hello, World!\n");
+int main() {
+     int x = 5;
+     int y = 10;
+     int sum = x + y;
+     printf("The sum of %d and %d is %d\n", x, y, sum);
+     return 0;
 }

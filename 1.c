22858f09899d91138911dc461f1ff194794079e9
farisZidan <fariszidan30@gmail.com>
2025-06-26 22:39:33 +0700

    3

--- 1.c
+++ 1.c
@@ -5,5 +5,8 @@ int main() {
      int y = 10;
      int sum = x + y;
      printf("The sum of %d and %d is %d\n", x, y, sum);
+
+     float a = 3.14;
+     printf("The value of pi is approximately %-3f\n", a);
      return 0;
-}
+} 
